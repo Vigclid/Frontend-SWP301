b14453621c54696fbb5183bad383f54343b92f3e
Đoàn Nam Sơn <namson1821@gmail.com>
2025-02-18 19:12:53 +0700

    copy

--- src/API/TagAPI/GET.tsx
+++ src/API/TagAPI/GET.tsx
@@ -1,34 +1,31 @@
-import { Tag } from '../../Interfaces/TagInterface'
-import axios from 'axios'
+import { Tag } from "../../Interfaces/TagInterface";
+import axios from "axios";
 
+const tagurl = "http://localhost:7233/api/Tag/";
+const tagsbyarturl = "http://localhost:7233/api/artworks/";
 
-const tagurl = "http://localhost:7233/api/Tag/"
-const tagsbyarturl = "http://localhost:7233/api/artworks/"
 export async function GetTagList() {
-        try{
-            let tagList:Tag[] = await axios.get(tagurl).then(response => response.data)
-            return tagList
-            
-        }catch(err){
-          console.log(err)
-        }
+  try {
+    let tagList: Tag[] = await axios.get(tagurl).then((response) => response.data);
+    return tagList;
+  } catch (err) {
+    console.log(err);
+  }
 }
-export async function GetTagById(id:string) {
-    try{
-        let tag:Tag = await axios.get(tagurl+id).then(response => response.data)
-        return tag
-        
-    }catch(err){
-      console.log(err)
-    }
+export async function GetTagById(id: string) {
+  try {
+    let tag: Tag = await axios.get(tagurl + id).then((response) => response.data);
+    return tag;
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-export async function GetTagByArtId(id:string) {
-  try{
-      let tag:Tag[] = await axios.get(tagsbyarturl+id+"/tags").then(response => response.data)
-      return tag
-      
-  }catch(err){
-    console.log(err)
+export async function GetTagByArtId(id: string) {
+  try {
+    let tag: Tag[] = await axios.get(tagsbyarturl + id + "/tags").then((response) => response.data);
+    return tag;
+  } catch (err) {
+    console.log(err);
   }
 }
